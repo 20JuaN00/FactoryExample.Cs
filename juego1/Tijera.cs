ea341fab8c8dcee5e6fe87da7f40180013048193
juan gomez <20juandiego00@gmail.com>
2025-04-04 13:04:52 -0500

    Ejemplo de fabrica con juego ya finalizado

---
+++ juego1/Tijera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juego1
+{
+    internal class Tijera : jugada
+    {
+
+        public string url { get; set; }
+
+        public string ataque(string contricante)
+        {
+            if (contricante == "Tijera")
+            {
+                return "EMPATO";
+            }
+
+            else if (contricante == "Papel")
+            {
+                return "GANO";
+            }
+
+            else
+            {
+                return "PERDIO";
+            }
+        }
+    }
+}
+
---
+++ juego1/jugada.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juego1
+{
+    internal interface jugada
+    {
+        //Almacenado de las imagenes
+        public string url { get; set; }
+        String ataque(String contricante);
+        
+
+        
+    }
+}
